09dde0cc8a087ed233f9c35cfb21d90110358526
Alphathur <1274221961@qq.com>
2021-01-24 13:26:35 +0800

    replace lib.request with requests, add dynamic cookie for wenku-pv-robot.py, and except all exception for every pv url

--- csdn-pv-robot.py
+++ csdn-pv-robot.py
@@ -3,24 +3,28 @@
 import random
 import re
 import time
-import urllib.request
+import traceback
 
+import requests
 from bs4 import BeautifulSoup
 
-# 这里覆盖你的csdn博客用户名
-USER_NAME = "m0_38106923"
+# 这里覆盖你的csdn博客用户ID
+USER_ID = "m0_38106923"
 
-opener = urllib.request.build_opener()
-opener.addheaders = [('User-agent',
-                      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36')]
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
+}
 
 
 def find_all_url():
-    url = "http://blog.csdn.net/" + USER_NAME + "/article/list"
-    html = opener.open(url).read().decode('utf-8')
-    end_url = "/" + USER_NAME + "/article/details/\d*"
+    url = "http://blog.csdn.net/" + USER_ID + "/article/list"
+    resp = requests.get(url, headers=headers)
+    if resp.status_code != 200:
+        return None
+    resp_text = requests.get(url, headers=headers).text
+    end_url = "/" + USER_ID + "/article/details/\d*"
     p = re.compile(end_url)
-    urls = list(set(p.findall(html)))  # 去重
+    urls = list(set(p.findall(resp_text)))  # 去重
     base_url = "http://blog.csdn.net"  # 合并地址
     for i in range(len(urls)):
         urls[i] = base_url + urls[i]
@@ -31,25 +35,27 @@ def csdn_visit(urls):
     for index, page in enumerate(urls):
         try:
             print(str(index), page)
-            page_content = opener.open(page).read().decode('utf-8')
+            resp = requests.get(page, headers=headers)
+            if resp.status_code != 200:
+                print('Invalid url:', page, "; current response:", resp)
+                continue
+            page_content = requests.get(page, headers=headers).text
             soup = BeautifulSoup(page_content, features="html.parser")
             blog_title = str(soup.title.string)
-            blog_title = blog_title[0:blog_title.rfind('-')]
+            blog_title = blog_title[0:blog_title.rfind('_')]
             print(blog_title)
             time.sleep(random.randint(1, 3))
-        except urllib.error.HTTPError:
-            print('urllib.error.HTTPError')
+        except Exception as e:
+            print(traceback.format_exc())
             time.sleep(3)
-        except urllib.error.URLError:
-            print('urllib.error.URLError')
-            time.sleep(5)
 
 
 if __name__ == '__main__':
-    all_url = find_all_url()
-    if not len(all_url):
-        print("请检查用户名是否正确")
     while True:
+        all_url = find_all_url()
+        if all_url is None or not len(all_url):
+            print("请检查用户ID是否正确")
+            break
         csdn_visit(all_url)
         print("开始休息...")
         time.sleep(random.randint(60, 360))
--- wenku-pv-robot.py
+++ wenku-pv-robot.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import json
 import math
 import random
 import time
-import urllib.request
+import traceback
 
+import requests
 from bs4 import BeautifulSoup
 
 # 这里覆盖你的百度文库商店ID
 SHOP_ID = "f3b219e8b8f67c1cfad6b85c"
 
-opener = urllib.request.build_opener()
-opener.addheaders = [('User-agent',
-                      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'),
-                     ('Cookie',
-                      'BAIDUID=A2C8E006AAB89C9C54DB98D08E8F34A7:FG=1')]
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
+}
+cookies = None
+
+
+def find_all_page():
+    global cookies
+    page = "https://wenku.baidu.com/user/interface/shopsearchdoc?shopId=" + SHOP_ID + "&pn=0&rn=12&docClass=-1&sortType=2"
+    resp = requests.get(page, headers=headers)
+    if resp.status_code != 200:
+        return None
+    cookies = resp.cookies.get_dict()
+    total_count = resp.json()['data']['totalCount']
+    return math.ceil(total_count / 12)
 
 
 def wenku_visit(pn):
     page = "https://wenku.baidu.com/user/interface/shopsearchdoc?shopId=" + SHOP_ID + "&pn=" + str(pn) + "&rn=12&docClass=-1&sortType=2"
-    res = opener.open(page).read().decode('utf-8')
-    js = json.loads(res)
+    js = requests.get(page, headers=headers).json()
+    time.sleep(2)
     content = js['data']['content']
     if not len(content):
         return
@@ -29,29 +39,28 @@ def wenku_visit(pn):
         try:
             url = "https://wenku.baidu.com/view/" + con['sourceId']
             print(str(index), con['title'], url)
-            page_content = opener.open(url).read()
+            resp = requests.get(url, headers=headers, cookies=cookies)
+            if resp.status_code != 200:
+                print('Invalid url:', url, "; current response:", resp)
+                continue
+            page_content = resp.content
             soup = BeautifulSoup(page_content, features="html.parser")
             blog_title = str(soup.title.string)
             print(blog_title)
             time.sleep(random.randint(1, 3))
-        except urllib.error.HTTPError:
-            print('urllib.error.HTTPError')
+        except Exception as e:
+            print(traceback.format_exc())
             time.sleep(3)
-        except urllib.error.URLError:
-            print('urllib.error.URLError')
-            time.sleep(5)
 
 
 if __name__ == '__main__':
-    page = "https://wenku.baidu.com/user/interface/shopsearchdoc?shopId=" + SHOP_ID + "&pn=0&rn=12&docClass=-1&sortType=2"
-    res = opener.open(page).read().decode('utf-8')
-    js = json.loads(res)
-    total_count = js['data']['totalCount']
-    total_page = math.ceil(total_count / 12)
-    print(total_page)
-    time.sleep(5)
     while True:
+        total_page = find_all_page()
+        if total_page is None or total_page <= 0:
+            print("请检查SHOP_ID")
+            break
+        time.sleep(2)
         for i in range(total_page):
             wenku_visit(i)
-            print("开始休息...")
-            time.sleep(random.randint(60, 360))
+        print("开始休息...")
+        time.sleep(random.randint(60, 360))
